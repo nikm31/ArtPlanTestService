ec0bad48f4753e317674f2c0fbd984bc73bab2cc
Николай <firewather@gmail.com>
2022-07-06 10:56:18 +0300

    Initial commit

--- src/main/java/ru/artplan/artplantest/repositories/AttemptRepository.java
+++ src/main/java/ru/artplan/artplantest/repositories/AttemptRepository.java
@@ -1,11 +1,13 @@
 package ru.artplan.artplantest.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.artplan.artplantest.model.Attempt;
 import ru.artplan.artplantest.model.User;
 
 import java.util.Optional;
 
+@Repository
 public interface AttemptRepository extends JpaRepository<Attempt, Long> {
 
     Optional<Attempt> findByUser(User user);
